aa4fbdd56cd7f65f9a6fea5b66c9a3c88dbedb60
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-11-18 16:52:14 +0530

    changes for deploy server

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 ssh facile@134.209.158.136 <<EOF 
-    cd ~/hsnotifyapi
-    git pull
-    npm install
-    npm run build
-    npm run winserve
-    exit
+ cd ~/hsnotifyapi
+ git pull
+ npm install
+ npm run build
+ npm run winserve
+ exit
 EOF
