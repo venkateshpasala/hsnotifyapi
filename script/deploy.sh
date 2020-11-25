da5c4da9a43af62c4c9cf490ad878f3059d2d07f
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-11-25 16:52:13 +0530

    changes for test deploy server

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-ssh centos@34.220.141.160 <<EOF 
- cd ~/workspace/hsnotifyapi
+ ssh -t -t centos@54.148.113.240 <<EOF 
+ cd ~/hsnotifyapi
+ pwd
+ git checkout main
  git pull
  npm install
  npm run build
- pm2 restart index.js --watch
+ pm2 delete index.js 
+ pm2 start index.js
  exit
 EOF
+
+
