f23c3d6336207c803b4cbbfff55a852077c21459
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-11-25 17:11:48 +0530

    changes for deploy server

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,15 +2,15 @@
 
  echo "Deploy sh file started"
  ssh -t -t centos@54.148.113.240 <<EOF 
- cd ~/hsnotifyapi
- pwd
- git checkout main
- git pull
- npm install
- npm run build
- pm2 delete index.js 
- pm2 start index.js
- exit
+    cd ~/hsnotifyapi
+    pwd
+    git checkout main
+    git pull
+    npm install
+    npm run build
+    pm2 delete index.js 
+    pm2 start index.js
+    exit
 EOF
 
 
