b89d09021754ebc59ee8ba673837b83cb4c7e196
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-11-26 16:21:22 +0530

    changes for server deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
- ssh -t -t centos@54.148.113.240 <<EOF 
-    cd ~/hsnotifyapi
-    pwd
-    git checkout main
-    git pull
-    npm install
-    npm run build
-    pm2 delete index.js 
-    pm2 start index.js
-    exit
-EOF
-
+ ssh -t -t facile@139.59.36.62 
 
