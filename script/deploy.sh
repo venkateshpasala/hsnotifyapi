52e6e6c5b9a30f703d89e195bd90c4e07eb10d31
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-11-26 16:41:36 +0530

    changes for deploy server on host

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
- ssh -t -t facile@139.59.36.62 
+ ssh -t -t facile@139.59.36.62 <<EOF 
+  cd ~/hsnotifyapi
+  pwd
+  git checkout main
+  git pull
+  npm install
+  npm run build
+  pm2 delete index.js 
+  pm2 start index.js
+  exit
+EOF
+
 
